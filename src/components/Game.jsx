e44b2de5d7a407143155e0b9e0a3437338afcb17
Alexander Khramov <hramovsaku@icloud.com>
2023-03-29 15:47:38 +0300

    commit OX Game

---
+++ src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from './Board';
+
+import './Game.css';
+
+
+const Game = () => {
+    return (
+        <div className="wrapper">
+            <Board/>
+        </div>
+    );
+}
+
+export default Game;
---
+++ src/components/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Square.css';
+
+const Square = () => {
+    return (
+        <button className="square">x</button>
+    );
+}
+
+export default Square;
